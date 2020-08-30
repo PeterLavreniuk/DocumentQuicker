20de5c159d7ce4060bea44efbf57754f8f53946d
PeterLavreniuk <peter.lavreniuk@gmail.com>
2020-08-30 10:46:25 +0300

    new migration - for renamed property.

---
+++ DocumentQuicker.DataProvider/Migrations/20200830074134_RenamedProperty.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DocumentQuicker.DataProvider.Migrations
+{
+    public partial class RenamedProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BankDescription",
+                table: "Banks");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Banks",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Banks");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BankDescription",
+                table: "Banks",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
